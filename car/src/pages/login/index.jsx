7f736a1c0ca84a1ef281284e0402fe31e168fa35
muhammadali shuhratjonov <muhammadalishuhratjonov50@gmail.com>
2024-10-17 16:26:11 +0500

    added new changes

--- car/src/pages/login/index.jsx
+++ car/src/pages/login/index.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import "./register.css";
 import { NavLink } from "react-router-dom";
 
-export const Register = () => {
+export const Login = () => {
   return (
     <div className="container">
       <form className="register">
-        <h2 className="register-title">Register</h2>
-        <input type="text" placeholder="UserName" className="register-input" />
+        <h2 className="register-title">Login</h2>
         <input type="email" placeholder="Email" className="register-input" />
         <input
           type="password"
           placeholder="Password"
           className="register-input"
         />
-        <NavLink to={"/login"} className="register-link">
-          Login
+        <NavLink to={"/register"} className="register-link">
+          Register
         </NavLink>
-        <button className="register-btn">register</button>
+        <button className="register-btn">login</button>
       </form>
     </div>
   );
